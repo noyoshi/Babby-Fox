d2188b91f8d3dc32b06adae63527225a20da1f22
Mike Eisemann <android441kitkat@gmail.com>
2018-07-27 15:13:00 -0400

    Initial Commit

---
+++ Babby Fox/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Babby Fox
+//
+//  Created by Michael Eisemann on 7/27/18.
+//  Copyright © 2018 Mike Eisemann. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Babby Fox/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Babby Fox
+//
+//  Created by Michael Eisemann on 7/27/18.
+//  Copyright © 2018 Mike Eisemann. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
